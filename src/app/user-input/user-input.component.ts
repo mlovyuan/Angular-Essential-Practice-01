39c3c8b2dc59f920ec65b00c778f28bb0918f264
yuan <mlovyuan@yahoo.com.tw>
2024-08-04 22:50:01 +0800

    add calculate method in user-input and InvestmentInput model

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,13 +10,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  // angular 17 above
+  // calcuate = @output()
+  @Output() calcuate = new EventEmitter<InvestmentInput>();
   enteredInitialInvestment = "0";
   enteredAnnualInvestment = "0";
   enteredExpectedReturn = "5";
   enteredDuration = "10";
 
   onSubmit() {
-    console.log("submit!");
-
+    this.calcuate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    });
   }
 }
